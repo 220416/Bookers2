7aaa740591d1cf0f097af063b59296c630713d1f
EC2 Default User <ec2-user@ip-172-31-42-180.ap-northeast-1.compute.internal>
2023-04-09 10:11:11 +0000

    nineth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  
+  def new
+  @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(user_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end  
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:tetle, :image, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to:'homes#top'
   devise_for :users
+  root to: "homes#top"
+  get 'about' => 'homes#about'
   resources :users, only: [:show, :edit, :update]
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show]
   
-  get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
